1c2230f937fd82375e88056605736b3a627b8aaa
DLSWL <59249240+WSTCYH@users.noreply.github.com>
2024-11-08 11:22:34 +0800

    Update run_gui.sh

--- run_gui.sh
+++ run_gui.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export HF_HOME=huggingface
 export PYTHONUTF8=1
-
+source "$script_dir/venv/bin/activate"
 python gui.py "$@"
-
